7ab38282ba5740f70ef7aab200b7b6b421201b71
CEIBA\manuel.ponton <Manuel Ponton>
2022-07-28 12:54:12 -0500

    Range.go

--- Map/Map.go
+++ Map/Map.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	m := make(map[string]int)
+	m["jose"] = 14
+	m["pepito"] = 20
+	fmt.Println(m)
+	//Forma diferente de declararlo
+	temperature := map[string]int{
+		"Earth": 15,
+		"Mars":  -65,
+	}
+	fmt.Println(temperature)
+
+	//Recorrer un map
+	for i, v := range m {
+		fmt.Println(i, v)
+	}
+
+	//Encontrar un valor
+	value, ok := m["jose"] //Se usa ok para validar si existe en el diccionario
+	fmt.Println(value, ok)
 }
